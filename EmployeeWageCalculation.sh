8c7d8f2a6e0d82b8a71c168986b4312bfe508dea
kirtibhave1000@gmail.com <kirtibhave1000@gmail.com>
2020-01-23 18:37:21 +0530

    REFACTORED: for loop to while loop , ADDED: Constants

--- EmployeeWageCalculation.sh
+++ EmployeeWageCalculation.sh
@@ -1,22 +1,28 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-partTimeEmployee=1
-fullTimeEmployee=2
-empWagePerHr=20
-numWorkingDays=20
-totalSalary=0
 
-for(( day=1; day<=$numWorkingDays; day++ ))
+#CONSTANTS
+PART_TIME_EMPLOYEE=1
+FULL_TIME_EMPLOYEE=2
+NUM_OF_WORKING_DAYS=20
+EMP_WAGE_PER_HR=20
+MAX_HRS_IN_MONTH=100
+TOTAL_EMP_HRS=0
+TOTAL_WORKING_DAYS=0
+TOTAL_SALARY=0
+
+while [[ $TOTAL_EMP_HRS -lt $MAX_HRS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $NUM_OF_WORKING_DAYS ]]
 do
-employeeCheck=$((RANDOM%3));
-    case $employeeCheck in
-     	     $fullTimeEmployee)
+    ((TOTAL_WORKING_DAYS++))
+     employeeCheck=$((RANDOM%3))
+     case $employeeCheck in
+     	     $FULL_TIME_EMPLOYEE)
                  empHrs=8 ;;
-           $partTimeEmployee)
+           $PART_TIME_EMPLOYEE)
                  empHrs=4 ;;
            *)
                  empHrs=0 ;;
-esac
-     salary=$(($empHrs*$empWagePerHr))
-     totalSalary=$(($totalSalary+$salary))
+     esac
+        TOTAL_EMP_HRS=$(($empHrs*$TOTAL_EMP_HRS))
 done
+        TOTAL_SALARY=$(($TOTAL_EMP_HRS+$EMP_WAGE_PER_HR))
