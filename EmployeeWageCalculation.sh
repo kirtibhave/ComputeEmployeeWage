33023ddb6cf44e80e741d979a0c9a09f7651225f
kirtibhave1000@gmail.com <kirtibhave1000@gmail.com>
2020-01-23 19:27:28 +0530

    REFACTORED:code using function

--- EmployeeWageCalculation.sh
+++ EmployeeWageCalculation.sh
@@ -6,16 +6,15 @@ PART_TIME_EMPLOYEE=1
 FULL_TIME_EMPLOYEE=2
 NUM_OF_WORKING_DAYS=20
 EMP_WAGE_PER_HR=20
-MAX_HRS_IN_MONTH=100
-TOTAL_EMP_HRS=0
+MAX_HRS_IN_MONTH=4
 TOTAL_WORKING_DAYS=0
 TOTAL_SALARY=0
-
-while [[ $TOTAL_EMP_HRS -lt $MAX_HRS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $NUM_OF_WORKING_DAYS ]]
-do
-    ((TOTAL_WORKING_DAYS++))
-     employeeCheck=$((RANDOM%3))
-     case $employeeCheck in
+TOTAL_EMP_HRS=0
+#VARIABLES
+empHrs=0
+ function getWorkingHours()
+  {
+     case $1 in
      	     $FULL_TIME_EMPLOYEE)
                  empHrs=8 ;;
            $PART_TIME_EMPLOYEE)
@@ -23,6 +22,14 @@ do
            *)
                  empHrs=0 ;;
      esac
+     echo $empHrs
+   }
+
+while [[ $TOTAL_EMP_HRS -lt $MAX_HRS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $NUM_OF_WORKING_DAYS ]]
+do
+        ((TOTAL_WORKING_DAYS++))
+        empHrs="$( getWorkingHours $((RANDOM%3)) )"
         TOTAL_EMP_HRS=$(($empHrs*$TOTAL_EMP_HRS))
 done
         TOTAL_SALARY=$(($TOTAL_EMP_HRS+$EMP_WAGE_PER_HR))
+
