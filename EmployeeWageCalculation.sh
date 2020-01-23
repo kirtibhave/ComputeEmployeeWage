e2d2ca40643c170cefe5a75ffc3a9f1f224c962e
kirtibhave1000@gmail.com <kirtibhave1000@gmail.com>
2020-01-23 17:15:02 +0530

    ADDED:Calculated wage for month,REFACTORED:Added constant and variables

--- EmployeeWageCalculation.sh
+++ EmployeeWageCalculation.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-PartTimeEmployee=1
-FullTimeEmployee=2
-EmployeeCheck=$(( RANDOM%3 ));
+partTimeEmployee=1
+fullTimeEmployee=2
+employeeCheck=$(( RANDOM%3 ));
 empWagePerHr=20
-
-case $EmployeeCheck in
-     $FullTimeEmployee)
+numWorkingDays=20
+totalSalary=0
+for ((day=1;day<=$numWorkingDays; day++ ))
+do
+   case $employeeCheck in
+     $fullTimeEmployee)
                  empHrs=8 ;;
-     $PartTimeEmployee)
+     $partTimeEmployee)
                  empHrs=4 ;;
      *)
                  empHrs=0 ;;
 esac
      salary=$(($empHrs*$empWagePerHr));
+     totalSalary=$(($totalSalary+$salary));
+done
+
+
